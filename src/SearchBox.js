aeb9e802357b5adb9e3119314005d33a59125a25
Matan Chen <matanchen1>
2021-04-15 21:51:36 +0300

    1

---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchBox = () => {
+    return (
+        <div className="pa2">
+            <input
+                className="pa3, bg-dark-green bg-light-blue"
+                type='Search'
+                placeholder='search robots'/>
+        </div>
+    );
+}
+export default SearchBox;
