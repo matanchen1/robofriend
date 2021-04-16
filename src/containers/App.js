9a3fd207e429609d6500a465ad9f69a8724e2ae3
Matan Chen <matanchen1>
2021-04-16 14:40:59 +0300

    update 1 - last update (add Scroll opt)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 //da
 
-import CardList from "./CardList";
-import {robots} from "./robots";
-import SearchBox from "./SearchBox"
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox"
 import "./App.css";
+import Scroll from "../components/Scroll";
 
 // SMART COMPONENT
 class App extends Component {
@@ -34,18 +34,20 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
+        return (!this.state.robots.length) ?
+            <h1>Loading</h1> :
+            (
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
+
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </div>
             );
         }
-    }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Card from './Card';
-import CardList from './CardList';
 import 'tachyons';
-import {robots} from './robots'
-import App from "./App";
+import App from "./containers/App";
 
 ReactDOM.render(
     <App/>
